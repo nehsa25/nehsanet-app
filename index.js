939593cde4b395f8a701f884ce4042818cf17f1e
nehsa25 <jesse.stone@nehsa.net>
2024-04-08 18:31:06 -0700

    Changing port back to 80

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 const app = express();
 app.use(express.json());
 app.use(express.static('nehsanet/dist/nehsanet/browser'))
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
     console.log(`Listing on port ${port}`);
 });
