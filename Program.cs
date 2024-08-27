a6ad3911fce3dbacc936b393e3701e28a3142467
nehsa25 <jesse.stone@nehsa.net>
2024-08-26 21:04:35 -0700

    Added header for CSP

--- Program.cs
+++ Program.cs
@@ -1,17 +1,60 @@
-
-
-using System.Diagnostics;
-using Microsoft.Extensions.Logging.Console;
 using MySqlConnector;
+
+public class CSPMiddleware
+{
+    private readonly string _cspPolicy;
+
+    public CSPMiddleware(string cspPolicy)
+    {
+        _cspPolicy = cspPolicy;
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        context.Response.Headers.Append("Content-Security-Policy", _cspPolicy);
+        await next(context);
+    }
+}
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddScoped<CSPMiddleware>();
+        services.AddSingleton<string>("default-src 'self';");
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // ... other middleware configurations
+
+        app.UseMiddleware<CSPMiddleware>();
+        // Example middleware usage
+
+        // ... other middleware configurations
+    }
+}
 
 internal class Program
 {
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        string cspPolicy = "default-src 'self';";
+        services.AddScoped<CSPMiddleware>(serviceProvider => new CSPMiddleware(cspPolicy));
+    }
+
     private static void Main(string[] args)
     {
-        IHost host = Host.CreateDefaultBuilder(args).Build();
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+
+            }).Build();
         var logger = host.Services.GetRequiredService<ILogger<Program>>();
         logger.LogInformation("Host created and logging enabled.");
-        
+
         var builder = WebApplication.CreateBuilder(args);
 
         // Logging support
@@ -37,7 +80,7 @@ internal class Program
                               });
         });
 
-        // Add services to the container.
+        // middleware services
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddHealthChecks();
@@ -46,9 +89,11 @@ internal class Program
 
         // MySQL support
         builder.Services.AddMySqlDataSource(builder.Configuration.GetConnectionString("Default")!);
-        
+
         // Setup final app to run
         var app = builder.Build();
+
+        // set to use CORS
         app.UseCors(customOrigin);
         app.UseCors(localOrigin);
 
@@ -65,7 +110,7 @@ internal class Program
         app.UseRouting();
         app.MapControllers();
         app.UseSwagger();
-        app.UseSwaggerUI();  
+        app.UseSwaggerUI();
         app.UseHealthChecks("/health");
         app.UseStaticFiles(); // allow us to serve map images
 
