2d8fc439c25127927afe8ceb964b148b9385503e
nehsa25 <jesse.stone@nehsa.net>
2024-09-08 13:43:57 -0700

    Updated gitignore

--- publish.ps1
+++ publish.ps1
@@ -1,67 +1,17 @@
 
 $build = (get-date).ToString("yyyyMMddHHmm");
-$localBuild = "./dist/nehsanet/browser";
-$version = (get-date).ToString("yyyy-MM-dd HH:mm");
-$full_version = "<!-- ${version} -->";
-$index_location = "$localBuild/index.html";
 
-$val = read-host "Publish UI, API, Both? [UI/api/both]";
-if ($val.ToLower() -eq 'both' -or $val.ToLower() -eq 'api') {
-    write-host "Getting API ready...";
-    write-host "Building docker API image ${build}";
-    docker build . -t nehsa/nehsaapi:$build --platform linux/amd64;
-    $cmd = "docker run -it -p 22007:22007 -e ASPNETCORE_URLS='https://+:22007' -e ASPNETCORE_HTTPS_PORT=22007 -e ASPNETCORE_Kestrel__Certificates__Default__Password='password' -e ASPNETCORE_Kestrel__Certificates__Default__Path='/https/api.nehsa.net.pfx'  -v C:\certs:/https/ -v C:\src\mud_images:/app/wwwroot/ nehsa/nehsaapi:${build}";
-    write-host 'To run:';
-    write-host $cmd;
+write-host "Getting API ready...";
+write-host "Building docker API image ${build}";
+docker build . -t nehsa/nehsaapi:$build --platform linux/amd64;
+$addCert = "-e ASPNETCORE_Kestrel__Certificates__Default__Password='password' -e ASPNETCORE_Kestrel__Certificates__Default__Path='/https/api.nehsa.net.pfx'  -v C:\certs:/https/";
+$cmd = "docker run -it -p 22007:22007 -e ASPNETCORE_URLS='https://+:22007' -e ASPNETCORE_HTTPS_PORT=22007 $addCert -v C:\src\mud_images:/app/wwwroot/ nehsa/nehsaapi:${build}";
+ 
+write-host 'To run:';
+write-host $cmd;
 
-    # output to file
-    $cmd | out-file -filepath ./run-api.ps1;
-    write-host 'To run in the future: ./run-api.ps1';
-}
-
-if ($val.ToLower() -eq '' -or $val.ToLower() -eq 'ui' -or $val.ToLower() -eq 'both') {
-    set-location ./nehsanet; 
-
-    if ($val.ToLower() -eq 'both') {
-        write-host "Updating version.ts";
-        set-content -path "./src/version.ts" -value "export const version = { number: '$version' }";
-    }
-    else {
-        $val = read-host "Apply build number ${version}? [Y/n]";
-        if ($val.ToLower() -eq '' -or $val.ToLower() -eq 'y') {
-            write-host "Updating version.ts";
-            set-content -path "./src/version.ts" -value "export const version = { number: '$version' }";
-        }
-    }
-    
-    write-host "Removing old files at $localBuild";
-    remove-item -v $localBuild/* -recurse;
-    
-    write-host "Running: ng build --configuration production";
-    ng build --configuration production;
-
-    write-host "Updating sitemap";
-    if (Test-Path "./sitemap.xml") {
-        remove-item ./sitemap.xml
-    }
-
-    set-location C:\src\python-angular-sitemapper
-    python update.py
-    copy-item -v .\sitemap.xml C:\src\nehsanet-app\nehsanet\src\sitemap.xml
-    set-location C:\src\nehsanet-app\nehsanet
-    
-    write-host "Updating index.html and version.ts with version info: ${version}";
-    add-content -path $index_location -value $full_version;
-    
-    write-host "Building docker UI image ${build}";
-    docker build . -t nehsa/nehsanet:$build --platform linux/amd64;
-    
-    write-host "Pushing image to DockerHub...";
-    docker push nehsa/nehsanet:$build;
-    
-    write-host "UI ready for deployment.";
-
-    set-location ..
-}
+# output to file
+$cmd | out-file -filepath ./run-api.ps1;
+write-host 'To run in the future: ./run-api.ps1';
 
 write-host "Done!";
