d79eba8c9bddd1b4cfe68a3cb1eeaf1ddb1292b6
jesse.stone@nehsa.net <jesse.stone@nehsa.net>
2024-09-13 16:32:33 -0700

    Updated for linux environment

--- publish.ps1
+++ publish.ps1
@@ -4,8 +4,8 @@ $build = (get-date).ToString("yyyyMMddHHmm");
 write-host "Getting API ready...";
 write-host "Building docker API image ${build}";
 docker build . -t nehsa/nehsaapi:$build --platform linux/amd64;
-$addCert = "-e ASPNETCORE_Kestrel__Certificates__Default__Password='password' -e ASPNETCORE_Kestrel__Certificates__Default__Path='/https/api.nehsa.net.pfx'  -v C:\certs:/https/";
-$cmd = "docker run -it -p 22007:22007 -e ASPNETCORE_URLS='https://+:22007' -e ASPNETCORE_HTTPS_PORT=22007 $addCert -v C:\src\mud_images:/app/wwwroot/ nehsa/nehsaapi:${build}";
+$addCert = "-e ASPNETCORE_Kestrel__Certificates__Default__Password='password' -e ASPNETCORE_Kestrel__Certificates__Default__Path='/https/api.nehsa.net.pfx'  -v ~/certs:/https/";
+$cmd = "docker run -it -p 22007:22007 -e ASPNETCORE_URLS='https://+:22007' -e ASPNETCORE_HTTPS_PORT=22007 $addCert -v ~/mud_images:/app/wwwroot/ nehsa/nehsaapi:${build}";
  
 write-host 'To run:';
 write-host $cmd;
